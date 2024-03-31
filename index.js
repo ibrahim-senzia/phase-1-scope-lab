949352950395df0ef72d4b232d8d01608aeed45c
ibrahim senzia <ibrahimsenzia@gmail.com>
2024-03-31 09:09:20 +0300

    scope-lab completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+//declare CustomerName to be bob
+var customerName = 'bob';
+
+//upperCaseCustomerName()
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase()
+}
+
+//setBestCustimer()
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+//overwriteBestCustomer()
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+//changeLeastFavoriteCustomer()
+const leastFavoriteCustomer = 'no one'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'someone'
+}
